75168e2aff8b254e67e0192cd585c1626397dd63
Andros Fenollosa <andros@fenollosa.email>
2020-03-04 21:07:01 +0100

    update xml parse

--- src/glosa_disqus_import/core.clj
+++ src/glosa_disqus_import/core.clj
@@ -1,7 +1,24 @@
 (ns glosa-disqus-import.core
+  (:require
+   [clojure.xml :as xml]
+   [clojure.java.io :as io])
   (:gen-class))
 
+
+(defn parse-xml-file
+  [file]
+  (xml/parse (io/file (io/resource file))))
+
+(defn get-threads
+  [lists]
+  (let [all-threads (get-in lists [:content] [:tag :thread])]
+
+    (filter #(= (get-in % [:tag :isClosed]) false) all-threads)
+
+    )
+  )
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Run"
   [& args]
-  (println "Hello, World!"))
+  (println (get-threads (parse-xml-file "disqus.xml"))))
