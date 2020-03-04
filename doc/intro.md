7a96cbfd267185624c49438834ee7f33d5a1a15b
Andros Fenollosa <andros@fenollosa.email>
2020-03-04 20:27:51 +0100

    Add lein

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to glosa-disqus-import
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/glosa_disqus_import/core_test.clj
@@ -0,0 +1,7 @@
+(ns glosa-disqus-import.core-test
+  (:require [clojure.test :refer :all]
+            [glosa-disqus-import.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
