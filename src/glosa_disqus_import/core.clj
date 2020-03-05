2df5e2b24c297483fa85c229c0f996f9a209c1fa
Andros Fenollosa <andros@fenollosa.email>
2020-03-05 23:21:31 +0100

    last version

--- src/glosa_disqus_import/core.clj
+++ src/glosa_disqus_import/core.clj
@@ -5,24 +5,33 @@
    [clojure.walk :as walk]
    [clojure.java.io :as io]
    [clj-time.coerce :as c]
-   )
+   [cheshire.core :refer [generate-stream]])
   (:gen-class))
 
 (defn parse-xml-file
   "Parse file XML to hash-map"
   [file]
-  (xml-to-json/parse (xml/parse (io/file (io/resource file)))))
+  (xml-to-json/parse (xml/parse (io/file file))))
 
 (defn get-threads
   "Get all threads"
   [lists]
-  (let [all-threads (doall (-> lists :disqus :thread))]
-    (filter (fn [thread] (every? true? [(= (-> thread :isClosed) "false") (= (-> thread :isDeleted) "false")])) all-threads)))
+  (let [all-threads            (doall (-> lists :disqus :thread))
+        filter-threads-actives (filter (fn [thread] (every? true? [(= (-> thread :isClosed) "false") (= (-> thread :isDeleted) "false")])) all-threads)
+        remove-keys-threads    (map (fn [thread] (dissoc thread :id)) filter-threads-actives)
+        rename-keys-threads    (walk/postwalk-replace {(keyword "@dsq:id") :id} remove-keys-threads) ;; Rename all keys @dsp:id to :id
+        ]
+    (do rename-keys-threads)
+    ))
+
+(defn get-single-thread
+  "Get single threads"
+  [lists id]
+  (first (filter (fn [thread] (= id (-> thread :id))) (get-threads lists))))
 
 (defn get-posts
   "Get all posts"
   [lists]
-  ;; {:isDeleted false, :parent {:@dsq:id 4494152992}, :createdAt 2020-02-27T16:56:03Z, :author {:name Cruz Arnulfo Gonzalez Dzib, :username cruzarnulfogonzalezdzib, :isAnonymous false}, :isSpam false, :thread {:@dsq:id 3989649477}, :id nil, :@dsq:id 4812781235, :message <p>Pero es igual de espía que los de EU, no me deja acomodar, agregar o eliminar favoritos, tengo mi cuenta, y a cada rato me la desactiva para pedir datos personales.</p>}
   (let [all-posts         (doall (-> lists :disqus :post))
         filter-posts      (filter (fn [post] (every? true? [(= (-> post :isSpam) "false") (= (-> post :isDeleted) "false")])) all-posts)
         remove-keys-posts (->> filter-posts
@@ -31,14 +40,17 @@
                                (map (fn [post] (dissoc post :id)))
                                (map (fn [post] (update-in post [:author] dissoc :isAnonymous))) ;; Remove by Key two level
                                )
-        rename-keys-posts (walk/postwalk-replace {(keyword "@dsq:id") :id} remove-keys-posts) ;; Rename all keys @dsp:id to :id
+        rename-keys-posts (walk/postwalk-replace {(keyword "@dsq:id") :id} remove-keys-posts)                                   ;; Rename all keys @dsp:id to :id
         update-dates      (map (fn [post] (assoc post :createdAt (/ (c/to-long (-> post :createdAt)) 1000))) rename-keys-posts) ;; Datos UTC to Unixtime
+        update-author     (map (fn [post] (assoc post :author (-> post :author :name))) update-dates)                           ;; Update author
+        add-thread        (map (fn [post] (assoc post :thread (get-single-thread lists (-> post :thread :id)))) update-author)  ;; Add thread
+        update-thread-id  (map (fn [post] (assoc post :thread (-> post :thread :link))) add-thread)                             ;; Update id thread to :url
         ]
-    (doall update-dates)
-    )
-  )
+    (doall update-thread-id)))
 
 (defn -main
   "Run"
-  [& args]
-  (println (get-posts (parse-xml-file "disqus.xml"))))
+  [file]
+  (let [lists (parse-xml-file file)
+        posts (get-posts lists)]
+    (generate-stream posts (clojure.java.io/writer (str file ".json")))))
