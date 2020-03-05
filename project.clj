8d8c4ccb923cac4365c92247e43c803b6e8d30fc
Andros Fenollosa <andros@fenollosa.email>
2020-03-05 23:25:55 +0100

    version 1.0.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject glosa-disqus-import "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject glosa-disqus-import "1.0.0"
+  :description "Export XML Disqus to JSON"
+  :url "https://programadorwebvalencia.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
@@ -8,5 +8,5 @@
                  [clj-time "0.15.2"]
                  [cheshire "5.10.0"]]
   :main ^:skip-aot glosa-disqus-import.core
-  :target-path "target/%s"
+  :target-path "dist/%s"
   :profiles {:uberjar {:aot :all}})
